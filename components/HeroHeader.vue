47147c11dc19d02afb85f225bda7511bf3b96117
cherbourg@fub.fr <cherbourg@fub.fr>
2023-03-23 17:25:20 +0100

    change padding to the title

--- components/HeroHeader.vue
+++ components/HeroHeader.vue
@@ -1,8 +1,6 @@
 <template>
 <section class="bg-white background">
-  <div
-    class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
-  >
+  <div class="mx-auto max-w-screen-xl px-4 py-24 lg:flex lg:h-screen lg:items-center">
     <div class="mx-auto max-w-xl text-center">
       <h1 class="text-3xl font-extrabold sm:text-5xl">
         Pour retrouver un plaisir partagé
