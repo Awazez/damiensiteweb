376f1325256048e2e66462682b4467105b789639
cherbourg@fub.fr <cherbourg@fub.fr>
2023-03-26 19:14:41 +0200

    add texte accompagnement

--- pages/accompagnement.vue
+++ pages/accompagnement.vue
@@ -7,36 +7,24 @@
   <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
       <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
           <header class="mb-4 lg:mb-6 not-format">
-              <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">Vision intégrale et intégrative de la santé </h1>
+              <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">Accompagnement </h1>
           </header>
-          <p class="lead text-justify">La vision intégrale et intégrative de la santé permet d’analyser une problématique de manière globale et multidimensionnelle.</p> <br>
-          <p class="lead text-justify">Cela permet de proposer un accompagnement adapté grâce à une compréhension globale de la problématique ou bien mettre en cohérence les différents outils et acteurs de santé. </p> <br>
-          <p class="lead text-justify">La vision intégrative de la santé s’appuie sur deux outils de lecture complémentaires: les quatre cadrans de Ken Wilber et la spirale dynamique de l’évolution de Clare Graves. </p> <br>
-          <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">“La vision intégrale”: les quatre cadrans</h1>
+          <p class="lead text-justify">Je suis un homme qui a à cœur de révéler le meilleur de chacun.e de vous. </p> <br>
+          <p class="lead text-justify">Mon objectif principal est d’être une opportunité de changement pour vous. Je vous accompagne à mettre du sens et de la compréhension sur les éléments bloquants qui animent votre vie. Pour cela, j’utilise plusieurs outils thérapeutiques pour vous aider à traverser vos zones d’ombres et de lumière avec bienveillance et sécurité. </p> <br>
+          <p class="lead text-justify">Mon accompagnement vous permet de revisiter votre histoire et de vous la réapproprier afin d’en faire quelque chose de bien pour votre évolution.  </p> <br>
+          <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">Pour cela, il est nécessaire d’être capable de nommer et d’accueillir toutes les parties de soi, ses manques, ses souffrances, ses limites, de traverser ses émotions pour dégager toutes ses ressources intérieures et ses excellences afin de déployer tout son potentiel dans sa vie.</h1>
           
-          <p class="lead text-justify">Le praticien va explorer plusieurs dimensions de la problématique de la personne, à savoir: </p> <br>
-          <p class="lead text-justify">la dimension individuelle et la dimension collective </p> <br>
-          <p class="lead text-justify"> la dimension subjective et la dimension objective</p> <br>
+          <p class="lead text-justify">Je vous propose un accompagnement bienveillant, authentique et humain, adapté à votre rythme. Un endroit où il n’y a pas de tabou dans lequel vous pouvez parler librement.
+Je vous accueille peu importe votre identité, vos orientations affectives et sexuelles, vos problématiques, vos choix, vos pratiques; tout simplement en tant qu’être. </p> <br>
+          <p class="lead text-justify">Spécialisé en sexothérapie, j'accompagne les femmes, les hommes et les couples à (re)trouver une sexualité épanouie, à s’aimer pleinement et à se sentir en vie. </p> <br>
+          <p class="lead text-justify"> Des séances de thérapie en marchant dans la nature sont possibles. C’est une psychothérapie connectée à la nature au rythme de vos pas afin d’apporter du mouvement et stimuler les zones du cerveau relatives à la mémoire.</p> <br>
 
-<p class="lead text-justify"> Il s’agit d’un outil simple qui permet de prendre en compte la globalité d’une problématique afin d’en cibler l’origine ou de comprendre les enjeux en présence. </p> <br>
+<p class="lead text-justify">Au plaisir que nos chemins se rencontrent ! </p> <br>
 <p class="lead text-justify">La vision intégrale de la santé a pour but de ne rien exclure et d’honorer toutes les dimensions de votre problématique, au service de votre mouvement évolutif.</p> <br>
           <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">“La vision intégrative”: la spirale dynamique </h1>
-<p class="lead text-justify">Il s’agit d’une théorie développée par Don Beck et Clare Graves, aux Etats-Unis dans les années 1980, faisant un lien entre l’histoire de l’humanité et l’histoire d’un individu. </p> <br>
-<p class="lead text-justify">L'idée est que le développement d’un individu rejoue en accéléré les grands mouvements évolutifs par lesquels l’humanité est passée depuis la sortie des grottes préhistoriques.
+<p class="lead text-justify">« Tu n’y verras clair qu’en regardant en toi. Qui regarde l’extérieur, rêve. Qui regarde en lui-même, s’éveille. » Carl Gustav Jung </p> <br>
+<p class="lead text-justify">« Soyons le changement que nous voulons voir dans ce monde. » Gandhi
  </p> <br>
-<p class="lead text-justify">Chaque stade évolutif (code couleur) apporte sa pertinence spécifique et propose des solutions nouvelles tout en incluant et transcendant les étapes précédentes. </p> <br>
-<p class="lead text-justify">Chaque code a sa propre vision du monde et un système de valeurs prédominant lui permettant de survivre. </p> <br>
-<p class="lead text-justify">Toutefois, chaque code a ses propres limites qui ne peuvent être résolues par le système qui les a créées. C'est pourquoi de nouveaux systèmes de valeurs apparaissent à travers des crises de passage afin de continuer l'évolution humaine. </p> <br>
-<p class="lead text-justify">De son côté, l'être humain se développe de la même manière, en se complexifiant dans son corps, ses pensées, ses émotions, ses vécus… traversant ainsi les mêmes étapes et systèmes de valeurs que l'humanité. </p> <br>
-<p class="lead text-justify">Tout comme l’humanité, l’humain vit également des crises qui le font évoluer et lui permettent d'aller vers du nouveau. </p> <br>
-<p class="lead text-justify">Chaque étape est prise dans une évolution et une complexification croissante et aucune n’est meilleure qu’une autre. </p> <br>
-<p class="lead text-justify">La spirale dynamique permet d’identifier à quel système de valeur un individu est le plus investi et ainsi lui proposer une écoute et un accompagnement respectant son modèle du monde. </p> <br>
-<p class="lead text-justify">Un praticien en vision intégrale éclaire les enjeux personnels de l’individu en fonction de sa structure et lui conseille les outils thérapeutiques les plus cohérents avec ses besoins. </p> <br>
-<p class="lead text-justify">De son côté, l'être humain se développe de la même manière, en se complexifiant dans son corps, ses pensées, ses émotions, ses vécus… traversant ainsi les mêmes étapes et systèmes de valeurs que l'humanité. </p> <br>
-<p class="lead text-justify">Ensemble, nous visitons les cristallisations qui peuvent avoir lieu à un ou plusieurs niveaux en y mettant de la conscience afin de libérer l’énergie qui y est bloquée pour redonner du mouvement à votre évolution. </p> <br>
-<p class="lead text-justify">L’idée est de pouvoir visiter ses propres codes et enjeux afin d’apaiser la relation qu’on a avec eux pour pouvoir ensuite y puiser les ressources nécessaires en fonction du contexte dans lequel nous nous trouvons.  </p> <br>
-<p class="lead text-justify">Grâce à cet accompagnement spécifique, vous pourrez identifier vos blocages, en retrouver les racines, revisiter et exprimer vos émotions afin de libérer vos blocages pour que vous puissiez retrouver votre autonomie et avancer sereinement dans votre vie. </p> <br>
-
 <br>
 <br>
 
